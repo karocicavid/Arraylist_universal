58d99f78dc788ab22fab25fd9e19d8c07a013756
karocicavid <69958924+karocicavid@users.noreply.github.com>
2020-10-19 18:38:55 +0300

    Update Program.cs

--- arraylist/ConsoleApp1/Program.cs
+++ arraylist/ConsoleApp1/Program.cs
@@ -1,36 +1,37 @@
-﻿using System;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 
 namespace cavka
-{   abstract class Person
+{
+    abstract class Person
     {
         public string name;
         public string surname;
-        public Person(string Name,string Surname)
+        public Person(string Name, string Surname)
         {
             name = Name; surname = Surname;
         }
     }
-    class Student:Person
+    class Student : Person
     {
         public int course;
         public int codebook;
         public Student(string Name, string Surname, int Course, int Codebook)
-            :base(Name, Surname)
+            : base(Name, Surname)
         {
             course = Course; codebook = Codebook;
         }
     }
-    class Aspirant:Person
+    class Aspirant : Person
     {
         public int course;
         public int codebook;
         public string disertation;
-        public Aspirant(string Name,string Surname,int Course,int Codebook,string Disertation)
-            :base(Name,Surname)
+        public Aspirant(string Name, string Surname, int Course, int Codebook, string Disertation)
+            : base(Name, Surname)
         {
-            course = Course; codebook = Codebook; disertation= Disertation;
+            course = Course; codebook = Codebook; disertation = Disertation;
         }
     }
     class Program
@@ -39,53 +40,59 @@ namespace cavka
         static ArrayList studentlist = new ArrayList();
         static void Main()
         {
-            Console.Write("If you wanna register as student press - 1 , as aspirant press - 2 : ");
-            int press12 = TryPress12();
-            switch (press12)
+            bool checki = true;
+            do
             {
-                case 1:
-                    {
-                        Console.Write("Input name of student : "); string name = Alphabet();
-                        Console.Write("Input surname of student : "); string surname = Alphabet();
-                        Console.Write("Input course of student : "); int course = TryPress();
-                        Console.Write("Input codebook of student : "); int codebook = TryPress();
-                        Student student = new Student(name, surname, course, codebook);
-                        studentlist.Add(student);
-                        break;
-                    }
-                case 2:
-                    {
-                        Console.Write("Input name of aspirant : "); string name = Alphabet();
-                        Console.Write("Input surname of aspirant : "); string surname = Alphabet();
-                        Console.Write("Input course of aspirant : "); int course = TryPress();
-                        Console.Write("Input codebook of aspirant : "); int codebook = TryPress();
-                        Console.Write("Input disertation of aspirant : "); string disertation = Console.ReadLine();
-                        Aspirant aspirant = new Aspirant(name, surname, course, codebook, disertation);
-                        aspirantlist.Add(aspirant);
-                        break;
-                    }
-            }
-            Console.Write("For continue  press - 1 , for see  users registered list and exit press - 2 : ");
-            int menu = TryPress12();
-            switch (menu)
-            {
-                case 1:
-                    {
-                        Main();
-                        break;
-                    }
-                case 2:
-                    {
-                        break;
-                    }
-            }
+                Console.Write("If you wanna register as student press - 1 :\nIf you wanna register as aspirant press - 2 : ");
+                int press12 = TryPress12();
+                switch (press12)
+                {
+                    case 1:
+                        {
+                            Console.Write("Input name of student : "); string name = Alphabet();
+                            Console.Write("Input surname of student : "); string surname = Alphabet();
+                            Console.Write("Input course of student : "); int course = TryPress();
+                            Console.Write("Input codebook of student : "); int codebook = TryPress();
+                            Student student = new Student(name, surname, course, codebook);
+                            studentlist.Add(student);
+                            break;
+                        }
+                    case 2:
+                        {
+                            Console.Write("Input name of aspirant : "); string name = Alphabet();
+                            Console.Write("Input surname of aspirant : "); string surname = Alphabet();
+                            Console.Write("Input course of aspirant : "); int course = TryPress();
+                            Console.Write("Input codebook of aspirant : "); int codebook = TryPress();
+                            Console.Write("Input disertation of aspirant : "); string disertation = Console.ReadLine();
+                            Aspirant aspirant = new Aspirant(name, surname, course, codebook, disertation);
+                            aspirantlist.Add(aspirant);
+                            break;
+                        }
+                }
+                Console.Write("\nFor continue  press - 1: \nFor see  users registered list press - 2 : ");
+                int menu = TryPress12();
+                switch (menu)
+                {
+                    case 1:
+                        {
+                            checki=true;
+                            break;
+                        }
+                    case 2:
+                        {
+                            checki = false;
+                            break;
+                        }
+                }
 
+            } while (checki ==true );
+       
 
             bool check = true;
 
             do
             {
-                Console.WriteLine("\n For get student with index press 1:\n For get aspirant with index press 2:\n For exit and get full list press 3:");
+                Console.WriteLine("\nFor get student with index press 1:\nFor get aspirant with index press 2:\nFor exit and get full list press 3:");
                 int presskey = TryPress();
                 switch (presskey)
                 {
@@ -96,17 +103,17 @@ namespace cavka
                             {
                                 Console.Write(" Input index of student : ");
                                 int index = TryPress12();
-                                if (index >= studentlist.Count) { Console.WriteLine("\nEmpty list,try again!");checkcase = true; }
+                                if (index >= studentlist.Count) { Console.WriteLine("\nEmpty list,try again!"); checkcase = true; }
                                 else
-                                { 
+                                {
                                     Student stud = (Student)studentlist[index];
-                                    Console.WriteLine( "Student name - {0} , surname - {1} , course - {2} , codebook - {3}",stud.name , stud.surname , stud.course, stud.codebook);
+                                    Console.WriteLine("Student name - {0} , surname - {1} , course - {2} , codebook - {3}", stud.name, stud.surname, stud.course, stud.codebook);
                                     Console.Write("For continue checking press - 1 , for go to menu - 2 : ");
                                     int num = TryPress12();
                                     if (num == 1) { checkcase = true; } else { checkcase = false; }
                                 }
                             } while (checkcase == true);
-                           break;
+                            break;
                         }
                     case 2:
                         {
@@ -118,8 +125,10 @@ namespace cavka
                                 if (index >= aspirantlist.Count) { Console.WriteLine("\nEmpty list,try again!"); checkcase = false; }
                                 else
                                 {
+                                    Console.ForegroundColor = ConsoleColor.Green;
                                     Aspirant asp = (Aspirant)aspirantlist[index];
                                     Console.WriteLine("Student name - {0} , surname - {1} , course - {2} , codebook - {3}", asp.name, asp.surname, asp.course, asp.codebook);
+                                    Console.ResetColor();
                                     Console.Write("For continue checking press - 1 , for go to menu - 2 : ");
                                     int num = TryPress12();
                                     if (num == 1) { checkcase = true; } else { checkcase = false; }
@@ -137,31 +146,36 @@ namespace cavka
 
             } while (check == true);
 
-          foreach (Aspirant each in aspirantlist)
-          {
-              Console.Write("\n Your aspirant : ");
-              Console.Write("Name of aspirant - " + each.name + ",surname - " + each.surname + ",course - " + each.course + ",codebook - " + each.codebook + ",disertation - " + each.disertation);
-          }
+            foreach (Aspirant each in aspirantlist)
+            {
+                Console.Write("\n Your aspirant : ");
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write("Name of aspirant - " + each.name + ",surname - " + each.surname + ",course - " + each.course + ",codebook - " + each.codebook + ",disertation - " + each.disertation);
+                Console.ResetColor();
+            }
 
-          foreach (Student each in studentlist)
-          {
-              Console.Write("\n Your student : ");
-              Console.Write("Name of student - " + each.name + ",surname - " + each.surname + ",course - " + each.course + ",codebook - " + each.codebook);
-          }
+            foreach (Student each in studentlist)
+            {
+                Console.Write("\n Your student : ");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write("Name of student - " + each.name + ",surname - " + each.surname + ",course - " + each.course + ",codebook - " + each.codebook);
+                Console.ResetColor();
+            }
+            Console.Read();
 
         }
         public static int TryPress12()
         {
-            int presskey=0;
-            if(int.TryParse(Console.ReadLine(),out presskey))
+            int presskey = 0;
+            if (int.TryParse(Console.ReadLine(), out presskey))
             { if (presskey == 1) { return presskey; } else if (presskey == 2) { return presskey; } else { Console.WriteLine("Wrong input try again!"); return TryPress12(); } }
-            else { Console.WriteLine("Wrong input try again!");return TryPress12(); }
+            else { Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine("Wrong input try again!"); Console.ResetColor(); return TryPress12(); }
         }
         public static int TryPress()
         {
             int presskey = 0;
-            if (int.TryParse(Console.ReadLine(), out presskey)) { return presskey;}
-            else { Console.WriteLine("Wrong input try again!"); return TryPress12(); }
+            if (int.TryParse(Console.ReadLine(), out presskey)) { return presskey; }
+            else { Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine("Wrong input try again!"); Console.ResetColor(); return TryPress(); }
         }
         public static string Alphabet()
         {
@@ -181,7 +195,7 @@ namespace cavka
                             else { check = false; }
                         }
                     }
-                    else { Console.WriteLine("Wrong name input again!"); check = false; break; }
+                    else { Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine("Wrong name input again!"); Console.ResetColor(); check = false; break; }
                     if (check == false) { Console.WriteLine("Wrong name input again!"); break; }
                 }
             } while (check == false);
